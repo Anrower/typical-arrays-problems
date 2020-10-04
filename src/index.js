9665ac3c0fb5c29b3d7324eac42fe25810efdc96
Anrower <anrowerdd@gmail.com>
2020-10-04 16:21:23 +0300

    feat: implement typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+    let isEmpty = arguments.length;
+    if (isEmpty === 0 || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let isEmpty = arguments.length;
+    if (isEmpty === 0 || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let isEmpty = arguments.length;
+    if (isEmpty === 0 || array.length === 0) {
+        return 0;
+    } else {
+        let sumAllChar = array.reduce((acc, item) => acc + item);
+        return sumAllChar / array.length;
+    }
+};
